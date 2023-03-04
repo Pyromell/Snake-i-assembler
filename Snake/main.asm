38cb22450558622a931d6694766baa9e4a0c70f6
Kaspar Eidlitz Rivera <kasei438@student.liu.se>
2023-03-04 12:02:28 +0100

    kompilerar

--- Snake/main.asm
+++ Snake/main.asm
@@ -1,8 +1,18 @@
+.equ    MOSI = PB3
+.equ    SCK  = PB5
+.equ    SS   = PB2
+.equ    B    = 0
+.equ    G    = 1
+.equ    R    = 2
+.equ	A    = 3
+.def    ZERO = r2
+ clr    ZERO
+
 .org 0x0000
-        jmp     PROG_START
+    jmp     PROG_START
 
 .org OVF0ADDR
-        jmp     MULTIPLEX
+    jmp     MULTIPLEX
 
 .include "init.inc"
 .include "vmem.inc"
@@ -19,22 +29,26 @@
 
 
 PROG_START:
+    ldi     r16,HIGH(RAMEND)
+    out     SPH,r16
+    ldi     r16,LOW(RAMEND)
+    out     SPL,r16
     call    INIT
 
 SETUP:
 ; Initiates player positions
     ldi     ZL,LOW(P1)
     ldi     ZH,HIGH(P1)
-    ldi     r16,4
-    st      Z,r16           
-    ldi     r17,15
+    ldi     r16,0
+    st      Z,r16
+    ldi     r17,0
     std     Z+1,r17
 
     ldi     ZL,LOW(P2)
     ldi     ZH,HIGH(P2)
     ldi     r16,15
-    st      Z,r16           
-    ldi     r17,0
+    st      Z,r16
+    ldi     r17,15
     std     Z+1,r17
 
     call    ERASE_VMEM

245f36ab24b85ba8e36f5196d9e152b3ad5d58cb
Johan Lindberg <Johanl9658@gmail.com>
2023-03-06 18:19:32 +0100

    Arbetade med kroppsflytten av ormarna och en startposition in main filen

--- Snake/main.asm
+++ Snake/main.asm
@@ -56,6 +56,34 @@ SETUP:
     ldi     r16,0b1111_1111
     st      Z,r16
 
+	/*-----Option for start positions
+
+	ldi		ZL,LOW(P1)
+	ldi		ZH,HIGH(P1)
+
+	ldi		r16,0b1101_0111  // head with x = 13, y = 7
+	st		Z,r16		
+	ldi		r16,0b1110_0111  // tail with x = 14, y = 7
+	std		Z+1,r16
+	ldi		r16,0b1111_0111  // tail with x = 15, y = 7
+	std		Z+2,r16
+	ldi		r16,3
+	sts		P1_LEN,r16		 // store lenght of the snake
+
+	ldi		ZL,LOW(P2)
+	ldi		ZH,HIGH(P2)
+
+	ldi		r16,0b0010_1000  // head with x = 2, y = 8
+	st		Z,r16		
+	ldi		r16,0b0001_1000  // tail with x = 1, y = 8
+	std		Z+1,r16
+	ldi		r16,0b0000_1000  // tail with x = 0, y = 8
+	std		Z+2,r16
+	ldi		r16,3
+	sts		P2_LEN,r16		 // store lenght of the snake
+
+	*/
+
     call    ERASE_VMEM
     call    UPDATE_VMEM
 

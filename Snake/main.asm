b276d49f491b23e6fcba5d189484d135d9eeb8c7
Kaspar Eidlitz Rivera <kasei438@student.liu.se>
2023-03-09 17:52:49 +0100

    städning, riktning, game over

--- Snake/main.asm
+++ Snake/main.asm
@@ -8,45 +8,58 @@
 .equ    R = 2
 .equ	A = 3
 ; Game constants
+.equ    SPEED = 10 ; lower is faster
 .equ	P1_COLOR = R
 .equ	P2_COLOR = B
 .equ	FRUIT_COLOR = G
-.equ	MAX_LEN  = 64
+.equ	MAX_LEN = 64
+.equ    P1_WINS = 1
+.equ    P2_WINS = 2
+.equ    TIED    = 3
 ; Controls
-.equ    J1X = 2
-.equ    J1Y = 3
-.equ    J2X = 0
-.equ    J2Y = 1
+.equ    JOY_1 = 2
+.equ    JOY_2 = 0
+.equ    RIGHT = 0
+.equ    UP    = 1
+.equ    LEFT  = 2
+.equ    DOWN  = 3
 ; Registers
 .def    ZERO = r2
 
-.org 0x0000
-    jmp     PROG_START
 
-.org OVF0ADDR
-    jmp     MULTIPLEX
-
-.include "help.inc"
-.include "init.inc"
-.include "vmem.inc"
-.include "mux.inc"
-.include "joystick.inc"
-
-.dseg
+.dseg ; SRAM
     VMEM:   .byte 128
     LINE:   .byte 1
 
     P1:     .byte MAX_LEN ; $xy
     P1_LEN: .byte 1
+    P1_DIR: .byte 1 ; $0b0000_dlur
 
     P2:     .byte MAX_LEN ; $xy
     P2_LEN: .byte 1
+    P2_DIR: .byte 1 ; $0b0000_dlur
 
-    FRUIT:  .byte 1	  ; $xy
+    FRUIT:  .byte 1	; $xy
+    SEED:   .byte 1 ; for random
 
-    SEED:   .byte 1 ; upconunt
+    STATUS: .byte 1
 .cseg
 
+
+.org 0x0000
+    jmp     PROG_START
+.org OVF0ADDR
+    jmp     MULTIPLEX
+
+
+; Includes
+.include "help.inc"
+.include "init.inc"
+.include "vmem.inc"
+.include "mux.inc"
+.include "joystick.inc"
+.include "move.inc"
+.include "ljud.inc"
 
 PROG_START:
     ldi     r16,HIGH(RAMEND)
@@ -54,50 +67,39 @@ PROG_START:
     ldi     r16,LOW(RAMEND)
     out     SPL,r16
     clr     ZERO
+
+    ; sets hardware, flags, clears memory
     call    INIT
-
-
-SETUP:
-; Initiates player positions
-    ldi		ZL,LOW(P2)
-	ldi		ZH,HIGH(P2)
-
-	ldi		r16,$d7  // head with x = 13, y = 7
-	st		Z,r16		
-	ldi		r16,$e7  // tail with x = 14, y = 7
-	std		Z+1,r16
-	ldi		r16,$f7  // tail with x = 15, y = 7
-	std		Z+2,r16
-	ldi		r16,3
-	sts		P1_LEN,r16		 // store lenght of the snake
-
-	ldi		ZL,LOW(P1)
-	ldi		ZH,HIGH(P1)
-
-	ldi		r16,$28  // head with x = 2, y = 8
-	st		Z,r16		
-	ldi		r16,$18  // tail with x = 1, y = 8
-	std		Z+1,r16
-	ldi		r16,$08  // tail with x = 0, y = 8
-	std		Z+2,r16
-	ldi		r16,3
-	sts		P2_LEN,r16		 // store lenght of the snake
-    
-
-    call    ERASE_VMEM
-    call    UPDATE_VMEM
-
-    sei ; interrupt enabled
-    call    RANDOM
-
+    ; sets start conditions, game mode
+    call    SETUP
 PLAY:
-	call    MOVE_BODYS
-    call    JOYSTICK_1
-    call    JOYSTICK_2
     call    ERASE_VMEM
     call    UPDATE_VMEM
+    call    CHECK_HITS
+    call    JOYSTICK
+	call    MOVE
     call    DELAY
-    call    HIT
-
+    ; game over?
+    lds     r16,STATUS
+    cp      r16,ZERO
+    brne    GAME_OVER
+    ; not over
     rjmp    PLAY
 
+GAME_OVER:
+    call    DELAY
+    call    DELAY
+    call    DELAY
+    call    DELAY
+
+    cpi     r16,TIED
+    breq    tied_screen
+    cpi     r16,P1_WINS
+    breq    p1_win_screen
+    cpi     r16,P2_WINS
+    breq    p2_win_screen
+
+tied_screen:
+p1_win_screen:
+p2_win_screen:
+    jmp     PROG_START

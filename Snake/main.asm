7adf237a1817e26c735a1c15f917f7ef54f8031d
Kaspar Eidlitz Rivera <kasei438@student.liu.se>
2023-03-11 21:16:26 +0100

    organisation

--- Snake/main.asm
+++ Snake/main.asm
@@ -6,13 +6,13 @@
 .equ    B = 0
 .equ    G = 1
 .equ    R = 2
-.equ	A = 3
+.equ    A = 3
 ; Game constants
 .equ    SPEED = 10 ; lower is faster
-.equ	P1_COLOR = R
-.equ	P2_COLOR = B
-.equ	FRUIT_COLOR = G
-.equ	MAX_LEN = 64
+.equ    P1_COLOR = R
+.equ    P2_COLOR = B
+.equ    FRUIT_COLOR = G
+.equ    MAX_LEN = 64
 .equ    P1_WINS = 1
 .equ    P2_WINS = 2
 .equ    TIED    = 3
@@ -39,7 +39,7 @@
     P2_LEN: .byte 1
     P2_DIR: .byte 1 ; $0b0000_dlur
 
-    FRUIT:  .byte 1	; $xy
+    FRUIT:  .byte 1     ; $xy
     SEED:   .byte 1 ; for random
 
     STATUS: .byte 1
@@ -78,7 +78,7 @@ PLAY:
     call    UPDATE_VMEM
     call    CHECK_HITS
     call    JOYSTICK
-	call    MOVE
+    call    MOVE
     call    DELAY
     ; game over?
     lds     r16,STATUS
@@ -92,7 +92,6 @@ GAME_OVER:
     call    DELAY
     call    DELAY
     call    DELAY
-
     lds     r16,STATUS
     cpi     r16,TIED
     breq    tied_screen
@@ -100,48 +99,27 @@ GAME_OVER:
     breq    p1_win_screen
     cpi     r16,P2_WINS
     breq    p2_win_screen
-
 tied_screen:
     call    ERASE_VMEM
-    ldi		ZH,HIGH(TIE_TAB*2)
-	ldi		ZL,LOW(TIE_TAB*2)
-    rjmp    WIN_PREP_DONE
-
+    ldi     ZH,HIGH(TIE_TAB*2)
+    ldi     ZL,LOW(TIE_TAB*2)
+    rjmp    win_prep_done
 p1_win_screen:
-	call	ERASE_VMEM
-	ldi		ZH,HIGH(RED_WIN_TAB*2)
-	ldi		ZL,LOW(RED_WIN_TAB*2)
-    rjmp    WIN_PREP_DONE
-
+    call    ERASE_VMEM
+    ldi     ZH,HIGH(RED_WIN_TAB*2)
+    ldi     ZL,LOW(RED_WIN_TAB*2)
+    rjmp    win_prep_done
 p2_win_screen:
-	call	ERASE_VMEM
-	ldi		ZH,HIGH(BLUE_WIN_TAB*2)
-	ldi		ZL,LOW(BLUE_WIN_TAB*2)
-
-WIN_PREP_DONE:
-	call	UPDATE_WIN_VMEM
-	call	DELAY
-	call	DELAY
-	call	DELAY
-	call	DELAY
-	call	DELAY
-	call	DELAY
-	call	DELAY
+    call    ERASE_VMEM
+    ldi     ZH,HIGH(BLUE_WIN_TAB*2)
+    ldi     ZL,LOW(BLUE_WIN_TAB*2)
+win_prep_done:
+    call    UPDATE_WIN_VMEM
+    call    DELAY
+    call    DELAY
+    call    DELAY
+    call    DELAY
+    call    DELAY
+    call    DELAY
+    call    DELAY
     jmp     PROG_START
-
-	
-RED_WIN_TAB: 
-	.db		$36, $37, $38, $39, $3A, $4A, $59, $58, $48, $47, $56, $56	//R 
-	.db		$76, $77, $78, $79, $7A, $8A, $88, $86, $9A, $98, $96, $96  //E
-	.db		$B6, $B7, $B8, $B9, $BA, $C6, $CA, $D7, $D8, $D9, $00, $00	//D
-	
-BLUE_WIN_TAB:
-	.db		$16, $17, $18, $19, $1A, $26, $28, $2A, $37, $39			//B
-	.db		$56, $57, $58, $59, $5A, $66, $76, $76						//L
-	.db		$96, $97, $98, $99, $9A, $A6, $B6, $B7, $B8, $B9, $BA, $BA	//U
-	.db     $D6, $D7, $D8, $D9, $DA, $E6, $E8, $EA, $F6, $F8, $FA, $00  //E
-TIE_TAB:
-	.db     $3A, $46, $47, $48, $49, $4A, $5A, $5A                      //T
-    .db     $76, $77, $78, $79, $7A, $7A                                //I
-    .db     $96, $97, $98, $99, $9A, $A6, $A8, $AA, $B6, $B8, $BA, $00  //E
-	
